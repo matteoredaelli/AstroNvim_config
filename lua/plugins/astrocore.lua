c3ba9dedd04c834a284a65610d884c7e5180db45
Matteo Redaelli <matteo.redaelli@gmail.com>
2025-08-20 15:56:25 +0200

    clipboard and tofu extension

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,4 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
@@ -29,6 +28,7 @@ return {
       -- see `:h vim.filetype.add` for usage
       extension = {
         foo = "fooscript",
+        tofu = "terraform",
       },
       filename = {
         [".foorc"] = "fooscript",
